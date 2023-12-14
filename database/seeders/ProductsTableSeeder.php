6c8b8a9fe785264b27c8f2da34bf4b5306cfdce0
Bepis1559 <axl2001@abv.bg>
2023-12-14 18:39:51 +0200

    added normal categoryNames in seed

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -13,6 +14,28 @@ class ProductsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory()->count(5)->create();
+        // Product::factory()->count(5)->create();
+
+        $categoryNames = [
+            'Cars',
+            'Hand Watches',
+            'Phones',
+            'Laptops',
+            'Cameras',
+            'Shoes',
+            'Furniture',
+            'Sunglasses',
+            'Books',
+            'Headphones',
+            'Fitness Equipment',
+            'Gaming Consoles',
+            'Jewelry',
+            'Home Appliances',
+            'Sports Gear',
+        ];
+
+        foreach ($categoryNames as $categoryName) {
+            Category::create(['name' => $categoryName]);
+        }
     }
 }
