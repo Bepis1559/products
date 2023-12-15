8defec9cc0948b185aeef2f5790ae0f64e7a4739
Bepis1559 <axl2001@abv.bg>
2023-12-15 20:58:12 +0200

    commented out the show method for Category in web.php

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/categories', [CategoryController::class, 'index'])->name('admin.categories.index');
     Route::get('/categories/create', [CategoryController::class, 'create'])->name('admin.categories.create');
     Route::post('/categories', [CategoryController::class, 'store'])->name('admin.categories.store');
-    Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('admin.categories.show');
+    // Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('admin.categories.show');
     Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
     Route::put('/categories/{id}', [CategoryController::class, 'update'])->name('admin.categories.update');
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name('admin.categories.destroy');
